cdbecc9524be5eb19e8e4715b4e41d210f78c7a3
shwetadubey23 <shwetadubey0246@gmail.com>
2022-09-20 13:39:22 +0530

    indexFile edit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const { createIntern } = require("../controllers/BooksController")
-const { createCollege, functionupInterns } = require("../controllers/UserController")
 
-
-
-router.post("/functionup/colleges", createCollege)
-router.post("/functionup/interns", createIntern)
-router.get("/functionup/collegeDetails", functionupInterns)
 
 
 
